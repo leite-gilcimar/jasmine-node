a6271e26d0cefcc1df7536ce7147f7145703bbde
C1321024 Gilcimar Soares Leite <c1321024@interno.bb.com.br>
2021-07-01 19:16:43 -0300

    Arquivos do projeto

---
+++ spec/exemplo/exemplo.spec.js
@@ -0,0 +1,10 @@
+describe("Suite de teste de exemplo com o NodeJS", () => {
+
+    it("Deve demonstrar o uso do Jasmine com o NodeJS", () => {
+        expect(true).toEqual(true);
+    })
+
+    it("Deve demonstrar o uso do karma em modo de escuta", () => {
+        expect(1).toEqual(1);
+    })
+})
